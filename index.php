14ec6f5a0c681fc7655f2974f89075d50773ec0a
ayberk duman <16290028@ogrenci.ankara.edu.tr>
2021-05-07 17:05:58 +0300

    Proje dosyası ekle.

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<head>
+  <meta charset="utf-8">
+  <title>Villain Pub</title>
+
+
+  <!-- Fontlar -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800|Montserrat:300,400,700" rel="stylesheet">
+
+  <!-- Bootstrap style -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- css lib -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+
+  <!-- style -->
+  <link href="css/style.css" rel="stylesheet">
+  <link href="img/favicon.jpg" rel="shortcut icon">
+
+
+</head>
+
+<body>
+    
+    <!-- main page -->
+    <section class="main-page">
+        <!-- left -->
+        <div class="left">
+            <div class="left-content">
+                <div>
+                <img src="img/favicon.jpg" alt="villainPub" width="180" height="180">
+                <h1 class="left-content-logoHeading">Villians need friends too!</h1>
+                </div>
+                <div>
+                <img src="img/ghosts.svg" alt="ghost" width="60" height="60">
+                <h3 class="left-content-heading">Find new friends, or old nemesis</h3>
+                </div>
+                <div>
+                <img src="img/frankenstein.svg" alt="frankenstein" width="60" height="60">
+                <h3 class="left-content-heading">Set up socialization</h3>
+                </div>
+                <div>
+                <img src="img/scarecrow.svg" alt="scarecrow" width="60" height="60">
+                <h3 class="left-content-heading">Look for new ways to commit attrocitries from like-minded peers</h3>
+                </div>
+            </div>
+        </div>
+        <!-- END left -->
+
+        <!-- right -->
+        <div class="right">
+            <div class="right-content-form">
+                        <form>
+                            <h2>Sign In</h2>
+                            <input type="text" name="" placeholder="Username" />
+                            <input type="password" name="" placeholder="Password" />
+                            <input type="submit" name="" value="Login" />
+                            <p class="signup">Don't have an account yet ? <a href="#" onclick="toggleForm();" >Sign up</a></p>
+                        </form>
+            </div>    
+        </div>
+        <!-- END right -->
+
+        
+     <!-- footer -->
+     <footer class="main-page-footer" >
+        <ul>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Terms</a></li>
+            <li><a href="#">Cookie Policy</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Developers</a></li>
+            <li><a href="#">Bussines</a></li>
+            <li><a href="#">Settings</a></li>
+            <b>@2021 VillianPub by Ayberk Duman</b>
+        </ul>
+     </footer>
+
+
+
+
+      <!-- END footer -->
+
+
+
+
+
+    </section>
+
+
+
+
+  <!-- lib -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/magnific-popup/magnific-popup.min.js"></script>
+  <script src="lib/sticky/sticky.js"></script>
+  <script src="js/main.js"></script>
+
+</body>
+</html>
